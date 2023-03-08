ce0672f017f26e40f333379dd42d8e6f583cd846
Luc <luchohieu@gmail.com>
2023-03-08 21:26:09 +0700

    update example html string

--- src/RTE.tsx
+++ src/RTE.tsx
@@ -8,6 +8,36 @@ import RTEControls from './RTEControls';
 import { useStyles } from './RTE.styles';
 import { useDisclosure } from '@mantine/hooks';
 
+const exampleHtml = /* html */ `
+    <div class="mantine-Paper-root mantine-2gyk2x" data-with-border="true">
+    <p>resizable image with responsive style</p>
+    <p style="text-align: center;">
+        <span class="node-imageComponent" data-image-component="true">
+            <img src="/NAILS.png"
+                alt="" 
+                width="200" 
+                height="200"
+                data-responsive="true"
+                data-image-component="true" 
+                style="width: 100%;height: 200px;object-fit: scale-down;"
+            >
+        </span>
+    </p>
+    <p>normal resizable image</p>
+    <p>
+        <span class="node-imageComponent" data-image-component="true">
+            <img src="/NAILS.png" 
+                alt="" 
+                width="300" 
+                height="200"
+                data-image-component="true">
+            <img src="/NAILS.png" alt="" width="100" height="100" data-image-component="true">
+        </span>
+        <img src="/NAILS.png" alt="" width="100" height="100" data-image-component="true">
+    </p>
+    <img src="/NAILS.png" alt="" width="100" height="100" data-image-component="true">
+`;
+
 const RTE = () => {
     const editor = useEditor({
         extensions: [
@@ -15,12 +45,7 @@ const RTE = () => {
             Image.Extension,
             TextAlign.configure({ types: ['heading', 'paragraph'] }),
         ],
-        content: `
-            <p>resizable image with responsive style</p>
-            <img src="/NAILS.png" data-image-component='true' data-responsive='true' />
-            <p>normal resizable image</p>
-            <img src="/NAILS.png" width="300" height="200" data-image-component='true' />
-        `,
+        content: exampleHtml,
     });
     const [showHtml, { toggle }] = useDisclosure();
 
@@ -34,7 +59,12 @@ const RTE = () => {
                     classNames={classes}
                     styles={{
                         content: {
+                            display: 'flex',
                             overflowX: 'auto',
+                            width: '100%',
+                            '& > div': {
+                                flexGrow: 1,
+                            },
                         },
                     }}
                 >
@@ -46,19 +76,19 @@ const RTE = () => {
                 </RichTextEditor>
                 {showHtml && <Overlay />}
             </Box>
-            <Switch onClick={toggle} label='Show HTML string' />
+            <Switch onClick={toggle} label='Show raw HTML string' />
             {showHtml && (
-                <>
-                    <Text>Raw HTML:</Text>
-                    <Paper sx={{ wordBreak: 'break-word' }} withBorder p={10}>
-                        {editor?.getHTML()}
-                    </Paper>
-                </>
+                <Paper sx={{ wordBreak: 'break-word' }} withBorder p={10} my={10}>
+                    {editor?.getHTML()}
+                </Paper>
             )}
             <Text>Result:</Text>
             <Paper
                 dangerouslySetInnerHTML={{
                     __html: editor?.getHTML() || '',
+                }}
+                sx={{
+                    overflowX: 'auto',
                 }}
                 className={classes.content}
                 withBorder
